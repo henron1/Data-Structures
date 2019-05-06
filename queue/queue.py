f9c750dd77fd2dfbdf5a7666ebc98e1348142c50
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-05-06 16:58:55 -0400

    implemented methods built in linked list calss to complete MVP ;

--- queue/queue.py
+++ queue/queue.py
@@ -11,21 +11,56 @@ class Node:
   
   def set_next(self, new_next):
     self.next_node = new_next
+  
+class LinkedList:
+  def __init__(self):
+    self.head = None
+    self.tail = None
+  
+  def add_to_tail(self, value):
+    new_node = Node(value)
+    if not self.head and not self.tail:
+      self.head = new_node
+      self.tail = new_node
+    else:
+      self.tail.set_next(new_node)
+      self.tail = new_node
+
+  def remove_head(self):
+    if not self.head and not self.tail:
+      return None
+
+    elif self.head == self.tail:
+      old_head = self.head
+      self.head = None
+      self.tail = None
+      return old_head.get_value()
     
+    else:
+      old_head = self.head
+      self.head = self.head.get_next()
+      return old_head.get_value()
+
+  def count(self):
+    current = self.head
+    count = 0
+    while current:
+      count += 1
+      current = current.get_next()
+    return count
 
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = list()
+    self.storage = LinkedList()
 
   def enqueue(self, item):
-    self.storage.insert(0, item)
-    self.size += 1
+    return self.storage.add_to_tail(item)
   
   def dequeue(self):
-    pass
+    return self.storage.remove_head()
 
   def len(self):
-    pass
+    return self.storage.count()
