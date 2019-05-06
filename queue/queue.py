b5fbbd07a6aece980c4f49840053fb1cb25dddd9
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-05-06 16:51:30 -0400

    using Node class to update queue

--- queue/queue.py
+++ queue/queue.py
@@ -1,12 +1,28 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+
+  def get_value(self):
+    return self.value
+
+  def get_next(self):
+    return self.next_node
+  
+  def set_next(self, new_next):
+    self.next_node = new_next
+    
+
 class Queue:
   def __init__(self):
     self.size = 0
     # what data structure should we
     # use to store queue elements?
-    self.storage = 
+    self.storage = list()
 
   def enqueue(self, item):
-    pass
+    self.storage.insert(0, item)
+    self.size += 1
   
   def dequeue(self):
     pass
