ea6587c204a373dea538bf7b521b15ec87cc5a96
Lambda_School_Loaner_98 <henry.neal0@gmail.com>
2019-05-08 16:19:59 -0400

    insert working on binary search tree

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -5,7 +5,17 @@ class BinarySearchTree:
     self.right = None
 
   def insert(self, value):
-    pass
+    if value < self.value: # is current node less than new node?
+      if not self.left: # if not left child node
+        self.left = BinarySearchTree(value) # insert new node
+      else:
+        self.left.insert(value)
+    else:
+      if not self.right:
+        self.right = BinarySearchTree(value)
+      else:
+        self.right.insert(value)
+      
 
   def contains(self, target):
     pass
